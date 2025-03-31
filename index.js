4408ca696b5a15eb5120e0cc497eb3e1a6d9439b
Ryo Igarashi <n33t5hin@gmail.com>
2025-03-31 13:18:01 +0900

    fix: Add bin field to the package.json

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
 import { pipeline } from "@huggingface/transformers";
 import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
